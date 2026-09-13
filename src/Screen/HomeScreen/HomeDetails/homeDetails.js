56bb8ec73af1248c12c7ac54ddb67d62f949c5fe
Vishal Gupta <vg4693@gmail.com>
2026-09-13 14:03:32 +0530

    OmniDev Fix: Analyze the git status, project dependencies, and codebase for any missing files, broken imports, or compilation errors. Recreate any deleted file or fix the broken references.

--- src/Screen/HomeScreen/HomeDetails/homeDetails.js
+++ src/Screen/HomeScreen/HomeDetails/homeDetails.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import './homeDetails.css'
+import React from 'react';
+import './homeDetails.css';
+
 const HomeDetails = () => {
   return (
     <div className="homeDetails">
-
       <div className="homeDetailLongCard">
         <div className="homeDetailLongCardTitle">Today’s Deals</div>
         <div className="homeDetailLongCardItems">
-
           <div className="scrollDiv">
-
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
               <div className="homeDetailLongCardItemImgDetail">
-
                 <div className="homeDetailLongCardItemImgTopDetail">
                   <div className="homeDetailPercentageOff">
                     Up to 20% off
@@ -22,7 +19,6 @@ const HomeDetails = () => {
                     Limited Time Deal
                   </div>
                 </div>
-
                 <div className="bottomHomeDetail">
                   iQOO Z9 5G | Starting @17999 Includ…
                 </div>
@@ -30,9 +26,76 @@ const HomeDetails = () => {
             </div>
 
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
               <div className="homeDetailLongCardItemImgDetail">
+                <div className="homeDetailLongCardItemImgTopDetail">
+                  <div className="homeDetailPercentageOff">
+                    Up to 25% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
+                </div>
+                <div className="bottomHomeDetail">
+                  OnePlus Nord CE 4 5G | From ₹24,999
+                </div>
+              </div>
+            </div>
 
+            <div className="homeDetailLongCardItem">
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
+              <div className="homeDetailLongCardItemImgDetail">
+                <div className="homeDetailLongCardItemImgTopDetail">
+                  <div className="homeDetailPercentageOff">
+                    Up to 30% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
+                </div>
+                <div className="bottomHomeDetail">
+                  Realme Narzo 70 Pro 5G | Starting…
+                </div>
+              </div>
+            </div>
+
+            <div className="homeDetailLongCardItem">
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
+              <div className="homeDetailLongCardItemImgDetail">
+                <div className="homeDetailLongCardItemImgTopDetail">
+                  <div className="homeDetailPercentageOff">
+                    Up to 15% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
+                </div>
+                <div className="bottomHomeDetail">
+                  Samsung Galaxy M15 5G | Starting…
+                </div>
+              </div>
+            </div>
+
+            <div className="homeDetailLongCardItem">
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
+              <div className="homeDetailLongCardItemImgDetail">
+                <div className="homeDetailLongCardItemImgTopDetail">
+                  <div className="homeDetailPercentageOff">
+                    Up to 40% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
+                </div>
+                <div className="bottomHomeDetail">
+                  Redmi 13C 5G | Starting @10499
+                </div>
+              </div>
+            </div>
+
+            <div className="homeDetailLongCardItem">
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='deal' />
+              <div className="homeDetailLongCardItemImgDetail">
                 <div className="homeDetailLongCardItemImgTopDetail">
                   <div className="homeDetailPercentageOff">
                     Up to 20% off
@@ -41,145 +104,22 @@ const HomeDetails = () => {
                     Limited Time Deal
                   </div>
                 </div>
-
                 <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
+                  iQOO Neo 9 Pro 5G | Snapdragon 8 Gen 2
                 </div>
               </div>
             </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
           </div>
-
         </div>
       </div>
-
 
       <div className="homeDetailLongCard">
         <div className="homeDetailLongCardTitle">Today’s Offer</div>
         <div className="homeDetailLongCardItems">
-
           <div className="scrollDiv">
-
-            {/* please watch the video for code */}
-
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='offer' />
               <div className="homeDetailLongCardItemImgDetail">
-
                 <div className="homeDetailLongCardItemImgTopDetail">
                   <div className="homeDetailPercentageOff">
                     Up to 20% off
@@ -188,7 +128,6 @@ const HomeDetails = () => {
                     Limited Time Deal
                   </div>
                 </div>
-
                 <div className="bottomHomeDetail">
                   iQOO Z9 5G | Starting @17999 Includ…
                 </div>
@@ -196,29 +135,42 @@ const HomeDetails = () => {
             </div>
 
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-
-              {/* please watch the video for code */}
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='offer' />
               <div className="homeDetailLongCardItemImgDetail">
-
                 <div className="homeDetailLongCardItemImgTopDetail">
-                 {/* please watch the video for code */}
+                  <div className="homeDetailPercentageOff">
+                    Up to 35% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
                 </div>
-
                 <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
+                  Laptops & Tablets | Up to 35% Off
                 </div>
               </div>
             </div>
 
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='offer' />
               <div className="homeDetailLongCardItemImgDetail">
+                <div className="homeDetailLongCardItemImgTopDetail">
+                  <div className="homeDetailPercentageOff">
+                    Up to 50% off
+                  </div>
+                  <div className="limitedTimeDealhomeDetail">
+                    Limited Time Deal
+                  </div>
+                </div>
+                <div className="bottomHomeDetail">
+                  Smartwatches | Starting ₹999
+                </div>
+              </div>
+            </div>
 
+            <div className="homeDetailLongCardItem">
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='offer' />
+              <div className="homeDetailLongCardItemImgDetail">
                 <div className="homeDetailLongCardItemImgTopDetail">
                   <div className="homeDetailPercentageOff">
                     Up to 20% off
@@ -227,63 +179,33 @@ const HomeDetails = () => {
                     Limited Time Deal
                   </div>
                 </div>
-
                 <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
+                  Headphones & Earbuds | Up to 60% Off
                 </div>
               </div>
             </div>
 
             <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              {/* please watch the video for code */}
-            </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
+              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" alt='offer' />
               <div className="homeDetailLongCardItemImgDetail">
-
                 <div className="homeDetailLongCardItemImgTopDetail">
                   <div className="homeDetailPercentageOff">
-                    Up to 20% off
+                    Up to 45% off
                   </div>
                   <div className="limitedTimeDealhomeDetail">
                     Limited Time Deal
                   </div>
                 </div>
-
                 <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
+                  Smart TVs | Up to 45% Off
                 </div>
               </div>
             </div>
-
-            <div className="homeDetailLongCardItem">
-              <img className='homeDetailLongCardItemImg' src="https://m.media-amazon.com/images/I/41WnWm3IjiL._AC_SY200_.jpg" />
-              <div className="homeDetailLongCardItemImgDetail">
-
-                <div className="homeDetailLongCardItemImgTopDetail">
-                  <div className="homeDetailPercentageOff">
-                    Up to 20% off
-                  </div>
-                  <div className="limitedTimeDealhomeDetail">
-                    Limited Time Deal
-                  </div>
-                </div>
-
-                <div className="bottomHomeDetail">
-                  iQOO Z9 5G | Starting @17999 Includ…
-                </div>
-              </div>
-            </div>
-
           </div>
-
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default HomeDetails
+export default HomeDetails;
