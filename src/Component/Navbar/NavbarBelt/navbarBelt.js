1e3d353da682754b945d49d78ac81cbb642665e4
Vishal Gupta <vg4693@gmail.com>
2026-09-13 14:03:16 +0530

    OmniDev Fix: Analyze the git status, project dependencies, and codebase for any missing files, broken imports, or compilation errors. Recreate any deleted file or fix the broken references.

--- src/Component/Navbar/NavbarBelt/navbarBelt.js
+++ src/Component/Navbar/NavbarBelt/navbarBelt.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './navbarBelt.css';
-import amazonLogo from '../../../Assets/amazonLogo.png';
-import india from '../../../Assets/india.png';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
-
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const NavbarBelt = () => {
-
     const cartItems = useSelector((state) => state.cart.items);
+    const amazonLogo = 'https://pngimg.com/uploads/amazon/amazon_PNG11.png';
+    const india = 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg';
 
     return (
         <div className='navbarBelt'>
@@ -22,7 +20,15 @@ const NavbarBelt = () => {
                     <span className='navbar_inLogo'>.in</span>
                 </Link>
 
-                {/* please watch the video for code */}
+                <div className="navbarBeltLocation">
+                    <div className="navbarBeltLocationImg">
+                        <LocationOnOutlinedIcon className='navbarBeltLocationImgIcon' sx={{ fontSize: "20px" }} />
+                    </div>
+                    <div className="navbarBeltLocationPlace">
+                        <div className="navbarBeltLocationTop">Delivering to Mumbai 400001</div>
+                        <div className="navbarBeltLocationBottom">Update location</div>
+                    </div>
+                </div>
             </div>
             <div className="navbarBeltSearchBox">
                 <div className="navbarBeltSearchDiv">
@@ -31,30 +37,35 @@ const NavbarBelt = () => {
                         <ArrowDropDownOutlinedIcon sx={{ fontSize: "20px" }} />
                     </div>
 
-                    {/* please watch the video for code */}
-
+                    <input type="text" className="navbarBeltInputSearchBox" placeholder="Search Amazon.in" />
+                    <Link to={'/products'} className="searchIconNavbarBelt">
+                        <SearchIcon className='searchIconNavbarBeltIcon' />
+                    </Link>
                 </div>
             </div>
             <div className="rightSideNavbarBelt">
                 <div className="indianFlagCode">
-                    {/* please watch the video for code */}
+                    <img src={india} className='indiaFlag' alt='indiaFlag' />
+                    <div className="indiaCodeNavbarBelt">EN <ArrowDropDownOutlinedIcon className='indiaCodeNavbarBeltDrp' sx={{ fontSize: "16px" }} /></div>
                 </div>
 
-                {/* please watch the video for code */}
+                <div className="helloSignInNavbaeBelt">
+                    <div className="helloTopNavbarBelt">Hello, sign in</div>
+                    <div className="indiaCodeNavbarBelt">Account & Lists <ArrowDropDownOutlinedIcon className='indiaCodeNavbarBeltDrp' sx={{ fontSize: "16px" }} /></div>
+                </div>
+
+                <div className="helloSignInNavbaeBelt">
+                    <div className="helloTopNavbarBelt">Returns</div>
+                    <div className="indiaCodeNavbarBelt">& Orders</div>
+                </div>
 
                 <Link to={'/cart'} className="helloSignInNavbaeBelt">
                     <span className='cartItemNumberNavbarBelt'>{cartItems.length}</span>
                     <div className="helloTopNavbarBelt"><ShoppingCartOutlinedIcon/> <span className='cartTitle'>Cart</span></div>
-                    
-
                 </Link>
-                
-
-
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default NavbarBelt
+export default NavbarBelt;
